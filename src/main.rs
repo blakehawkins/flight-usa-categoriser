df8ef77e6d35b5f7782f2b0620d66e49f9f99226
blakehawkins <blake.hawkins.11@gmail.com>
2020-08-21 12:12:25 +0100

    rework matcher

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,74 @@
-use std::io::Read;
-
+use oops::Oops;
 use phf::phf_map;
+use stdinix::stdinix;
 
 #[derive(Debug, PartialEq)]
 enum Category {
     InUsa,
-    NotInUsa
+    NotInUsa,
 }
 
+use crate::Category::*;
+
 static CATEGORISATION: phf::Map<&'static str, Category> = phf_map! {
-    "AGP" => Category::NotInUsa,
-    "AMS" => Category::NotInUsa,
-    "BAH" => Category::NotInUsa,
-    "BCN" => Category::NotInUsa,
-    "BGY" => Category::NotInUsa,
-    "BKK" => Category::NotInUsa,
-    "BRU" => Category::NotInUsa,
-    "BUD" => Category::NotInUsa,
-    "CIA" => Category::NotInUsa,
-    "CRL" => Category::NotInUsa,
-    "DMK" => Category::NotInUsa,
-    "EDI" => Category::NotInUsa,
-    "EMA" => Category::NotInUsa,
-    "EWR" => Category::InUsa,
-    "GRU" => Category::NotInUsa,
-    "HEL" => Category::NotInUsa,
-    "HKT" => Category::NotInUsa,
-    "HND" => Category::NotInUsa,
-    "JFK" => Category::InUsa,
-    "KBV" => Category::NotInUsa,
-    "KIX" => Category::NotInUsa,
-    "LAX" => Category::InUsa,
-    "LGW" => Category::NotInUsa,
-    "LHR" => Category::NotInUsa,
-    "LIS" => Category::NotInUsa,
-    "LPX" => Category::NotInUsa,
-    "LTN" => Category::NotInUsa,
-    "MVD" => Category::NotInUsa,
-    "NRT" => Category::NotInUsa,
-    "OSL" => Category::NotInUsa,
-    "RIX" => Category::NotInUsa,
-    "SFO" => Category::InUsa,
-    "STN" => Category::NotInUsa,
-    "SVO" => Category::NotInUsa,
-    "TLS" => Category::NotInUsa,
-    "TXL" => Category::NotInUsa,
+    "AGP" => NotInUsa,
+    "AMS" => NotInUsa,
+    "BAH" => NotInUsa,
+    "BCN" => NotInUsa,
+    "BGY" => NotInUsa,
+    "BKK" => NotInUsa,
+    "BRU" => NotInUsa,
+    "BUD" => NotInUsa,
+    "CIA" => NotInUsa,
+    "CRL" => NotInUsa,
+    "DMK" => NotInUsa,
+    "EDI" => NotInUsa,
+    "EMA" => NotInUsa,
+    "EWR" => InUsa,
+    "GRU" => NotInUsa,
+    "HEL" => NotInUsa,
+    "HKT" => NotInUsa,
+    "HND" => NotInUsa,
+    "IAD" => InUsa,
+    "JFK" => InUsa,
+    "KBV" => NotInUsa,
+    "KIX" => NotInUsa,
+    "LAX" => InUsa,
+    "LGW" => NotInUsa,
+    "LHR" => NotInUsa,
+    "LIS" => NotInUsa,
+    "LPX" => NotInUsa,
+    "LTN" => NotInUsa,
+    "MVD" => NotInUsa,
+    "NRT" => NotInUsa,
+    "OSL" => NotInUsa,
+    "RIX" => NotInUsa,
+    "SFO" => InUsa,
+    "STN" => NotInUsa,
+    "SVO" => NotInUsa,
+    "TLS" => NotInUsa,
+    "TXL" => NotInUsa,
 };
 
 fn main() -> Result<(), std::io::Error> {
-    let mut buf = String::new();
+    stdinix(|line| {
+        let airports = line.trim().split(',').map(str::trim).collect::<Vec<_>>();
 
-    std::io::stdin().lock().read_to_string(&mut buf)?;
+        assert_eq!(airports.len(), 2);
 
-    buf.trim().split('\n').for_each(|line| {
-        let mut matches = line
-            .trim()
-            .split(',')
-            .map(|st| (st, CATEGORISATION.get(st.trim())))
-            .filter(|opt| opt.1.is_some())
-            .map(|pair| (pair.0, pair.1.unwrap()));
+        let matches = airports
+            .iter()
+            .map(|airport| {
+                CATEGORISATION
+                    .get(airport.trim())
+                    .oops(&format!("Airport {} not known", airport))
+            })
+            .collect::<Result<Vec<_>, _>>()?;
 
-        if matches.clone().count() != 2 {
-            eprintln!("!! {:?}", matches);
+        if matches[0] != matches[1] {
+            println!("{},{}", airports[0], airports[1]);
         }
 
-        let pair = (matches.next().unwrap(), matches.next().unwrap());
-
-        if (pair.0).1 != (pair.1).1 {
-            println!("{}\t{}", (pair.0).0, (pair.1).0);
-        }
-    });
-
-    Ok(())
+        Ok(())
+    })
 }
