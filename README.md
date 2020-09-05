9eb41a90d09b63ee399ef61df57985ec30c22c8e
blakehawkins <blake.hawkins.11@gmail.com>
2020-09-05 16:31:49 +0100

    add more airports; add support for long-form airport names via export

--- README.md
+++ README.md
@@ -3,9 +3,10 @@ For tax purposes I need a recording of when I was in the united states. I use my
 I wrote a quick program that categorises airports as "in" or "out" of the united states, and I pipe in data from flightradar to work out when I entered and left the USA.
 
 ```
-$ xsv select From,To data.tsv | tail -n +2 | cargo run | xargs -I {} grep "{}" data.tsv
+$ xsv sort data.csv | xsv select From,To | tail -n +2 | cargo run
     Finished dev [unoptimized + debuginfo] target(s) in 0.43s
      Running `target/debug/flight-usa-categoriser`
-2019-09-21      UA901   N2331U  SFO     LHR     5,359   12:25   06:55   UAL     B77W
+JFK,HEL
+HEL,JFK
 ...
 ```
--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,29 @@ enum Category {
 use crate::Category::*;
 
 static CATEGORISATION: phf::Map<&'static str, Category> = phf_map! {
+    "ABZ" => NotInUsa,
     "AGP" => NotInUsa,
     "AMS" => NotInUsa,
+    "ATH" => NotInUsa,
     "BAH" => NotInUsa,
+    "BJV" => NotInUsa,
     "BCN" => NotInUsa,
     "BGY" => NotInUsa,
     "BKK" => NotInUsa,
     "BRU" => NotInUsa,
     "BUD" => NotInUsa,
+    "CDG" => NotInUsa,
+    "CHQ" => NotInUsa,
     "CIA" => NotInUsa,
+    "CPH" => NotInUsa,
     "CRL" => NotInUsa,
     "DMK" => NotInUsa,
+    "DOH" => NotInUsa,
+    "DXB" => NotInUsa,
     "EDI" => NotInUsa,
     "EMA" => NotInUsa,
     "EWR" => InUsa,
+    "GLA" => NotInUsa,
     "GRU" => NotInUsa,
     "HEL" => NotInUsa,
     "HKT" => NotInUsa,
@@ -42,13 +51,20 @@ static CATEGORISATION: phf::Map<&'static str, Category> = phf_map! {
     "MVD" => NotInUsa,
     "NRT" => NotInUsa,
     "OSL" => NotInUsa,
+    "PHX" => InUsa,
     "RIX" => NotInUsa,
+    "SAN" => InUsa,
     "SFO" => InUsa,
     "STN" => NotInUsa,
     "SVO" => NotInUsa,
+    "TLL" => NotInUsa,
     "TLS" => NotInUsa,
     "TXL" => NotInUsa,
 };
+
+fn shorten(airport: &str) -> &str {
+    airport.split('(').nth(1).and_then(|part| part.split('/').nth(0)).unwrap_or_else(|| airport)
+}
 
 fn main() -> Result<(), std::io::Error> {
     stdinix(|line| {
@@ -61,14 +77,20 @@ fn main() -> Result<(), std::io::Error> {
             .map(|airport| {
                 CATEGORISATION
                     .get(airport.trim())
+                    .or_else(|| CATEGORISATION.get(shorten(airport)))
                     .oops(&format!("Airport {} not known", airport))
             })
             .collect::<Result<Vec<_>, _>>()?;
 
         if matches[0] != matches[1] {
-            println!("{},{}", airports[0], airports[1]);
+            println!("{},{}", shorten(airports[0]), shorten(airports[1]));
         }
 
         Ok(())
     })
+}
+
+#[test]
+fn test() {
+    assert_eq!(shorten("Los Angeles / Los Angeles International (LAX/KLAX)"), "LAX");
 }
